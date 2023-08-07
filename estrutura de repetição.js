9dad41482fe277ae20b0f336f4a31bd2c55cb051
SargentoPimenta <141243829+SargentoPimenta@users.noreply.github.com>
2023-08-07 11:12:17 -0300

    Add files via upload

---
+++ estrutura de repetição.js
@@ -0,0 +1,31 @@
+//tabuada de 5 em for
+console.log("tabuada de 5")
+for(let i = 1; i<=10; i++) {
+let valor=i*5
+console.log('5 x' , i , "=" , valor)}
+
+
+//PAR ou ÍMPAR
+console.log("tabuada de 5")
+let valor
+let divisão = 2
+
+for(let i = 1; i<=10; i++) {
+valor=i*5
+console.log('5 x ' , i , "=" , valor,".")
+
+if (valor % divisão == 0) {
+    console.log (`resultado da tabuada de 5 x ${i} é par`)
+} else {
+    console.log (`resultado da tabuada de 5 x ${i} é par`)
+}
+}
+
+
+
+
+
+
+
+
+
